59b505f1a2ea547e7460486d95b5f8332f4d4c7e
Aoi Iwaki <sanchunliu484@gmail.com>
2025-07-22 18:57:05 +0900

    20250722 - モデル作成 - 完了

---
+++ app/database/migrations/2025_07_22_090950_create_invites_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invites', function (Blueprint $table) {
+            $table->id();
+            $table->string('invite_code');
+            $table->datetime('used_at');
+            $table->tinyInteger('used_flag')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invites');
+    }
+};
---
+++ app/database/migrations/2025_07_22_091306_create_parents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', '30');
+            $table->string('nickname', '30');
+            $table->string('image', '100')->nullable();
+            $table->string('profile', '300')->nullable();
+            $table->string('password', '100');
+            $table->string('mailaddress', '50');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parents');
+    }
+};
